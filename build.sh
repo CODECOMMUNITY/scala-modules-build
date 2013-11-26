d86e356bc586c58abf2e77cafb59b6f7985695e6
Adriaan Moors <adriaan.moors@typesafe.com>
2013-11-26 13:03:15 -0800

    Align binary versions by interleaving core and module builds.
    All transitive dependencies of a scala release must consistently
    use the same binary version, or SBT (in principle, correctly) complains.
    
    This script:
     1. locally publishes Scala core (cf. the ant build's locker stage)
     2. locally publishes the modules built using that compiler (quick)
     3. publishes Scala built with locker, depending on quick modules,
        which were also built with locker (thus, all on the same binary version)
     4. bumps versions.properties to set STARR version to locker, commits it
     5. tests modules (can't test in step 2. get weird match error in sbt),
        and publishes them.
    
    It also generates tags for every project (except if `buildLocal` is `true`).
    
    Now "all that needs to be done" is make this bullet proof/automatable.
    (Use fresh maven repo, run on jenkins, push tags if all is well.)
    
    The weird error I saw when running 'test' task in step 2.
    (I think it's due to some hidden exception that causes class loading
    to fail.)
    
    ```
    scala.MatchError: com.novocode.junit.JUnitFramework@1f7a6f44 (of class com.novocode.junit.JUnitFramework)
    	at sbt.TestFramework.createFramework(TestFramework.scala:35)
    	at sbt.TestFramework.create(TestFramework.scala:52)
    ```

--- build.sh
+++ build.sh
@@ -1,70 +1,124 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
-        SCALA_VER="2.11.0-M7"
+     MAVEN_SUFFIX="-M8"
+        SCALA_VER="2.11.0$MAVEN_SUFFIX"
           XML_VER="1.0.0-RC7"
       PARSERS_VER="1.0.0-RC5"
    SCALACHECK_VER="1.11.1"
       PARTEST_VER="1.0.0-RC8"
 PARTEST_IFACE_VER="0.2"
 
-publishTask=publish-signed
+baseDir="/Users/adriaan/git/"
+
 buildLocal=true
+publishAnt=publish.local #publish.signed
+publishSbt=publish-local #publish-signed
+
+# TODO: clean local repo, or publish to a fresh one
 
 $buildLocal || (
-  echo "NOTE: THIS WIPES OUT ANY LOCAL CHANGES IN ~/git/scala ~/git/scala-partest-interface ~/git/scala-partest ~/git/scalacheck ~/git/scala-parser-combinators ~/git/scala-xml"
+  echo "NOTE: THIS WIPES OUT ANY LOCAL CHANGES IN $baseDir/scala $baseDir/scala-partest-interface $baseDir/scala-partest $baseDir/scalacheck $baseDir/scala-parser-combinators $baseDir/scala-xml"
   echo "Also, I assume you've cleaned your local ~/.ivy2 and ~/.m2 repos."
   echo "Press the any key to proceed"
   read
 )
 
-update() { git pull $1 $2 && git clean -fxd && git --no-pager show ; }
+update() {
+  $buildLocal && cd $baseDir/$2 && return 0
 
-# test and publish to sonatype, assuming you have ~/.sbt/0.13/sonatype.sbt and ~/.sbt/0.13/plugin/gpg.sbt
-cd ~/git/scala-xml && ($buildLocal || \
-  (update https://github.com/scala/scala-xml.git master && \
-   git tag -s "v$XML_VER" -m"Scala Standard XML Library v$XML_VER")) && \
-sbt 'set version := "'$XML_VER'"' \
-    'set scalaVersion := "'$SCALA_VER'"' \
-    clean test $publishTask publishM2
+  repo="https://github.com/$1/$2.git"
+  ref=${3-master}
 
-cd ~/git/scala-parser-combinators && ($buildLocal ||\
-  (update https://github.com/scala/scala-parser-combinators.git master && \
-   git tag -s "v$PARSERS_VER" -m"Scala Standard Parser Combinators Library v$PARSERS_VER")) && \
-sbt 'set version := "'$PARSERS_VER'"' \
-    'set scalaVersion := "'$SCALA_VER'"' \
-    clean test $publishTask publishM2
+  mkdir -p $baseDir && cd $baseDir
+  [[ -d $2 ]] || git clone $repo
+  cd $2
 
-cd ~/git/scalacheck && ($buildLocal || (update https://github.com/adriaanm/scalacheck.git master)) && \
-sbt 'set version := "'$SCALACHECK_VER'"' \
-    'set scalaVersion := "'$SCALA_VER'"' \
-    'set every scalaBinaryVersion := "'$SCALA_VER'"' \
-    'set VersionKeys.scalaParserCombinatorsVersion := "'$PARSERS_VER'"' \
-    clean test publish-local publishM2
+  git pull --rebase $repo $ref
 
-cd ~/git/scala-partest  && ($buildLocal ||\
-  (update https://github.com/scala/scala-partest.git master && git tag -s "v$PARTEST_VER" -m"Scala Partest v$PARTEST_VER")) && \
-sbt 'set version :="'$PARTEST_VER'"' \
-    'set scalaVersion := "'$SCALA_VER'"' \
-    'set VersionKeys.scalaXmlVersion := "'$XML_VER'"' \
-    'set VersionKeys.scalaCheckVersion := "'$SCALACHECK_VER'"' \
-    clean $publishTask publishM2
+  $buildLocal || (git fetch $repo $ref && git reset --hard FETCH_HEAD && git clean -fxd)
 
-cd ~/git/scala-partest-interface  && ($buildLocal ||\
-  (update https://github.com/scala/scala-partest-interface.git master && git tag -s "v$PARTEST_IFACE_VER"  -m"Scala Partest Interface v$PARTEST_IFACE_VER")) && \
-sbt 'set version :="'$PARTEST_IFACE_VER'"' \
-    'set scalaVersion := "'$SCALA_VER'"' \
-    clean $publishTask publishM2
+  git --no-pager show
+}
 
-# Sanity check: make sure the Scala test suite passes / docs can be generated with these modules.
-cd ~/git/scala #&& git checkout "v$SCALA_VER" && git clean -fxd
-ant -Dstarr.version=$SCALA_VER -Dstarr.use.released=1\
+tag() { $buildLocal || (git tag -s $1 -m$2) ; }
+
+publishModules() {
+  publishTask=$1
+
+  # test and publish to sonatype, assuming you have ~/.sbt/0.13/sonatype.sbt and ~/.sbt/0.13/plugin/gpg.sbt
+  update scala scala-xml
+  tag "v$XML_VER" "Scala Standard XML Library v$XML_VER"
+  sbt 'set version := "'$XML_VER'"' \
+      'set resolvers += Resolver.mavenLocal'\
+      'set scalaVersion := "'$SCALA_VER'"' \
+      clean $2 $publishTask publishM2
+
+  update scala scala-parser-combinators
+  tag "v$PARSERS_VER" "Scala Standard Parser Combinators Library v$PARSERS_VER"
+  sbt 'set version := "'$PARSERS_VER'"' \
+      'set resolvers += Resolver.mavenLocal'\
+      'set scalaVersion := "'$SCALA_VER'"' \
+      clean $2 $publishTask publishM2
+
+  update rickynils scalacheck $SCALACHECK_VER
+  sbt 'set version := "'$SCALACHECK_VER'"' \
+      'set resolvers += Resolver.mavenLocal'\
+      'set scalaVersion := "'$SCALA_VER'"' \
+      'set every scalaBinaryVersion := "'$SCALA_VER'"' \
+      'set VersionKeys.scalaParserCombinatorsVersion := "'$PARSERS_VER'"' \
+      clean $2 publish-local publishM2
+
+  update scala scala-partest
+  tag "v$PARTEST_VER" "Scala Partest v$PARTEST_VER"
+  sbt 'set version :="'$PARTEST_VER'"' \
+      'set resolvers += Resolver.mavenLocal'\
+      'set scalaVersion := "'$SCALA_VER'"' \
+      'set VersionKeys.scalaXmlVersion := "'$XML_VER'"' \
+      'set VersionKeys.scalaCheckVersion := "'$SCALACHECK_VER'"' \
+      clean $publishTask publishM2
+
+  update scala scala-partest-interface
+  tag "v$PARTEST_IFACE_VER" "Scala Partest Interface v$PARTEST_IFACE_VER"
+  sbt 'set version :="'$PARTEST_IFACE_VER'"' \
+      'set resolvers += Resolver.mavenLocal'\
+      'set scalaVersion := "'$SCALA_VER'"' \
+      clean $publishTask publishM2
+}
+
+update scala scala
+
+# publish core so that we can build modules with this version of Scala and publish them locally
+ant -Dmaven.version.suffix=$MAVEN_SUFFIX\
+    -Dscalac.args.optimise=-optimise\
+    -Ddocs.skip=1\
     -Dlocker.skip=1\
+    publish-core-local
+
+# build, test and publish modules with this core
+publishModules publish-local
+
+# Rebuild Scala with these modules so that all binary versions are consistent.
+# Update versions.properties to new modules.
+# Sanity check: make sure the Scala test suite passes / docs can be generated with these modules.
+# don't skip locker (-Dlocker.skip=1\), or stability will fail
+cd $baseDir/scala
+ant -Dmaven.version.suffix=$MAVEN_SUFFIX\
+    -Dscalac.args.optimise=-optimise\
+    -Dstarr.version=$SCALA_VER\
     -Dscala.binary.version=$SCALA_VER\
     -Dpartest.version.number=$PARTEST_VER\
     -Dscala-xml.version.number=$XML_VER\
     -Dscala-parser-combinators.version.number=$PARSERS_VER\
     -Dscalacheck.version.number=$SCALACHECK_VER\
-    test-opt docs.done
+    -Dupdate.versions=1\
+    test $publishAnt
+
+git commit versions.properties -m"Bump versions.properties for $SCALA_VER."
+
+tag "v$SCALA_VER" "Scala v$SCALA_VER"
+
+# rebuild modules for good measure
+publishModules $publishSbt test
 
 say "Woo-hoo\!"
 
